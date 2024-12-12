071ab9cf380caf7b7cc862df9f519c6f3acc8dc6
BlueeyezTheGreat <Flight0Of0Icarus@gmail.com>
2024-12-11 22:11:12 -0500

    i messed it all up

--- app/src/main/java/com/example/finalproject/CreateAccountActivity.kt
+++ app/src/main/java/com/example/finalproject/CreateAccountActivity.kt
@@ -1,3 +1,12 @@
+"""
+ * CreateAccountActivity.kt
+ * Android App Development - CSCI 380
+ * By Jordan Brown and Boden Kahn
+
+ * This file adds functionality for the creation of user accounts to eventually allow authentication
+ * into our menu app
+"""
+
 package com.example.finalproject
 
 import android.content.Intent
@@ -26,36 +35,57 @@ class CreateAccountActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Gets the text input edit texts from the layout and initializes firebase
         emailET = findViewById(R.id.tietEmail)
         passwordET = findViewById(R.id.tietPassword)
         passwordConfirmET = findViewById(R.id.tietPasswordConfirm)
         auth = FirebaseAuth.getInstance()
         val backButton = findViewById<Button>(R.id.backButton)
+
+        // Adds listener to the back button to return to the login screen if the user does not
+        // wish to create an account
         backButton.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        // Initializes and adds listener to sign up button
         val signUpButton = findViewById<Button>(R.id.signUpButton)
         signUpButton.setOnClickListener{
+
+            // Gets the text out of the email, password and password confirmation text inputs
             var email = emailET.text.toString()
             var password = passwordET.text.toString()
             var passwordConfirm = passwordConfirmET.text.toString()
+
+            // Ensures that the text inputs are not empty
             if (email.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty()){
+                // If empty shows a toast indicating the need to fill all text boxes
                 Toast.makeText(this, "Please fill in all required values", Toast.LENGTH_SHORT).show()
             }
+
+            // Compares the provided password and confirmation password to ensure equality
             if (password != passwordConfirm){
+                // If not equal prompts users to input matching passwords
                 Toast.makeText(this, "Passwords must match", Toast.LENGTH_SHORT).show()
                 passwordConfirmET.error = "Passwords must match"
             }
             else {
+                // If passwords were equal we attempt to add a new user to the firebase
+                // authentication tool
                 passwordConfirmET.error = null
                 auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
+                        // If successful we show a toast letting users know that their account was
+                        // added and return them to the login page to begin logging in
                         Toast.makeText(this, "Sign up successful", Toast.LENGTH_SHORT).show()
                         val intent = Intent(this, LoginActivity::class.java)
                         startActivity(intent)
                     }
                     else{
+                        // Otherwise if the account creation was unsuccessful we show a toast
+                        // informing users that their account could not be created
                         Toast.makeText(this, "Sign up unsuccessful", Toast.LENGTH_SHORT).show()
                     }
                 }
